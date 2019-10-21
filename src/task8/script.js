19e3c40b3b6bc95b9143f4cb27ae6f1c9db6697d
dsrtF0x-git <54433971+dsrtF0x-git@users.noreply.github.com>
2019-10-21 23:25:27 +0300

    Add files via upload

---
+++ src/task8/script.js
@@ -0,0 +1,6 @@
+function destroyElement () {
+  document.addEventListener('click', event => {
+    const elemToDestroy = document.elementFromPoint(event.clientX, event.clientY);
+    elemToDestroy.remove();
+  });
+}
